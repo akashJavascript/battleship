b007e6f4c5ddf9e39d1faaebb47bd85aabf25050
Akash <akashrangaswamy2010@gmail.com>
2024-03-09 12:35:01 -0800

    feat: recieveAttack can miss ships/ hit bigger ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,20 +2,45 @@ import Ship from './ship';
 
 function Gameboard() {
     const ships = [];
+    const missedAttacks = [];
     function placeShip(length, position) {
         const newShip = Ship(length);
-        ships.push([newShip, position]);
+        const positions = [];
+        for (let i = 0; i < length; i += 1) {
+            const newPosition = [position[0] + i, position[1]];
+            positions.push(newPosition);
+        }
+        ships.push({ ship: newShip, positions });
     }
     function receiveAttack(position) {
         let hit = false;
-        ships.forEach(([ship, shipPosition]) => {
-            if (JSON.stringify(shipPosition) === JSON.stringify(position)) {
-                ship.hit();
-                hit = ship.isSunk();
-            }
+        ships.forEach(({ ship, positions }) => {
+            positions.forEach((shipPosition, index) => {
+                if (JSON.stringify(shipPosition) === JSON.stringify(position)) {
+                    ship.hit(index);
+                    hit = true;
+                }
+            });
         });
+        allSunk(hit);
+        if (!hit) {
+            missedAttacks.push(position);
+        }
         return hit;
     }
-    return { placeShip, ships, receiveAttack };
+    function allSunk(hit) {
+        if (hit) {
+            let sunk = true;
+            ships.forEach(({ ship }) => {
+                if (!ship.isSunk()) {
+                    sunk = false;
+                }
+            });
+            if (sunk) {
+                return true;
+            }
+        }
+    }
+    return { placeShip, missedAttacks, receiveAttack };
 }
 export default Gameboard;
